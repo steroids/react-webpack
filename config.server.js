0a8e6d87523eb24e70e8f8476eb813e20a825bcf
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-06-29 12:16:32 +0700

    Fix document

--- config.server.js
+++ config.server.js
@@ -95,7 +95,7 @@ module.exports = ({config, baseUrl, cpus}) => {
             new webpack.ProvidePlugin({
                 window: path.resolve(path.join(__dirname, 'mock/window.mock')),
                 localStorage: path.resolve(path.join(__dirname, './mock/localStorage.mock')),
-                document: 'global/document',
+                document: {},
             }),
         ].filter(Boolean),
         performance: {
